78ed9993389ad92451d48a6af377f7ae1f13beed
Filipe Mariz Fernandes Ferreira da Costa <72476620+Falapas@users.noreply.github.com>
2021-05-07 21:06:52 +0100

    Update App.py

--- energy_price_simulator/App.py
+++ energy_price_simulator/App.py
@@ -1,7 +1,12 @@
+import numpy as np
+import pickle
 from flask import Flask, render_template, request, url_for, redirect, flash
 from flask_mysqldb import MySQL
+#from sklearn.tree import DecisionTreeRegressor
 
 app = Flask(__name__)
+
+model = pickle.load(open('Model.sav', 'rb'))
 
 # Mysql Connection
 app.config['MYSQL_HOST'] = 'localhost'
@@ -18,56 +23,37 @@ def Index():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM date')
     data = cur.fetchall()
+    cur.close()
     return render_template('index.html', date = data)
 
-@app.route('/add_contact', methods=['POST'])
-def add_contact():
+@app.route('/add_value', methods=['POST'])
+def add_value():
     if request.method == 'POST':
         year = request.form['year']
         month = request.form['month']
         day = request.form['day']
         hour = request.form['hour']
         cur = mysql.connection.cursor()
-        cur.execute('INSERT INTO date (year, month, day, hour) VALUES (%s, %s, %s, %s)', (year, month, day, hour))
+        
+        with open('Model.sav', 'rb') as f:
+            model = pickle.load(f)
+        features = np.array([445,318,4324,5025,154,0,1214,6183,7096,43,74,100,210,3118,29014,283,283,283,1036,84,5,256,0,275,275,275,1036,87,1,196,16,271,271,271,1005,75,1,224,0,280,280,280,1038,78,2,45,0,275,275,275,1021,81,1,220,0,year,month,day,hour])
+        #features = get_features(year, month, day, hour)
+        
+        price = model.predict(features.reshape(1, -1))
+        
+        cur.execute('INSERT INTO date (year, month, day, hour, price) VALUES (%s, %s, %s, %s, %s)', (year, month, day, hour, price[0]))
         mysql.connection.commit()
         flash('Date Added Successfully')
         return redirect(url_for('Index'))
 
-
-@app.route('/edit/<id>')
-def get_date(id):
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM date WHERE id = %s', (id))
-    data = cur.fetchall()
-    return render_template('edit-date.html', value = data[0])
-
-@app.route('/update/<id>', methods = ['POST'])
-def update_value(id):
-    if request.method == 'POST':
-        year = request.form['year']
-        month = request.form['month']
-        day = request.form['day']
-        hour = request.form['hour']
-    cur = mysql.connection.cursor()
-    cur.execute("""
-        UPDATE date
-        SET year = %s,
-            month = %s,
-            day = %s,
-            hour = %s
-        WHERE id = %s
-    """, (year, month, day, hour, id))
-    mysql.connection.commit()
-    flash('Value Updated Successfully')
-    return redirect(url_for('Index'))
-
-@app.route('/delete/<string:id>')
-def delete_contact(id):
+@app.route('/delete/<string:id>', methods = ['POST','GET'])
+def delete_value(id):
     cur = mysql.connection.cursor()
     cur.execute('DELETE FROM date WHERE id = {0}'.format(id))
     mysql.connection.commit()
     flash('Date Removed Successfully')
     return redirect(url_for('Index'))
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port = 3000, debug = True)
