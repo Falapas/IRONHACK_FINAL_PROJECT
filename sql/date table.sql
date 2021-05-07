14654be5b84d7cee7a9505a68418579766c1883d
Filipe Mariz Fernandes Ferreira da Costa <72476620+Falapas@users.noreply.github.com>
2021-05-07 21:06:07 +0100

    Create date table.sql

---
+++ sql/date table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `final_project`.`date` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `year` INT NULL,
+  `month` INT NULL,
+  `day` INT NULL,
+  `hour` INT NULL,
+  `price` FLOAT NULL,
+  PRIMARY KEY (`id`));
+
+SELECT * FROM final_project.date;
